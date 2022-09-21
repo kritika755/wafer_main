2fbc6fbf8f5fef42eb9845d52bba91a706fd34b6
kritika755 <kritiikaaa@gmail.com>
2022-09-21 19:07:30 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='It classifies if a wafer is default or not',
+    author='kritika',
+    license='MIT',
+)
